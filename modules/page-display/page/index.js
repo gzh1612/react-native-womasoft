7b9ae1bd10a48d860b6e1e614712a8d034439f05
gzh1612 <gzh1215@hotmail.com>
2020-07-17 14:03:52 +0800

    添加 scroll 页面滑动方法

--- modules/page-display/page/index.js
+++ modules/page-display/page/index.js
@@ -2,12 +2,12 @@ import page_render from "./page_render";
 import page_text from "./page_text";
 import page_a from "./page_a";
 import page_icon from './page_icon';
-import page_scroll from "./page_scroll";
+import page_slide from "./page_slide";
 
 export default {
     render: page_render,//页面最外层组件
     text: page_text,//简单的文本组件
     a: page_a,//简单的点击按钮
     icon: page_icon,//字体图标
-    scroll: page_scroll,//滚动
+    slide: page_slide,//滚动
 }
---
+++ modules/page-display/page/page_slide.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {TouchableHighlight, View, ScrollView, RefreshControl} from 'react-native';
+
+import theme from '../theme';
+
+export default class PageScroll extends Component {
+    constructor(props) {
+        super(props);
+        this.css = theme.get() ?? {};
+        this.state = {
+            children: props.children,
+            style: props.style,
+            onRefresh: props.onRefresh,
+            refreshing: props.refreshing ?? false,
+        }
+    }
+
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (nextProps.children === this.state.children &&
+            nextProps.refreshing === this.state.refreshing &&
+            JSON.stringify(nextProps.style) === JSON.stringify(this.state.style)) return false;
+        this.setState({
+            children: nextProps.children,
+            style: nextProps.style,
+            refreshing: nextProps.refreshing,
+        });
+        return true;
+    }
+
+    render() {
+        const stateStyle = this.state.style ?? {};
+        return <ScrollView refreshControl={
+            <RefreshControl refreshing={this.state.refreshing}
+                            onRefresh={() => {
+                                if (typeof this.state.onRefresh === 'function') this.state.onRefresh();
+                            }}/>}>
+            <TouchableHighlight activeOpacity={1}>
+                <View style={stateStyle}>
+                    {this.state.children}
+                </View>
+            </TouchableHighlight>
+        </ScrollView>
+    }
+}
