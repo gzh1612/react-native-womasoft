84a24cf3027a16df21a5b35aacc8e79361c16ddf
gzh1612 <gzh1215@hotmail.com>
2020-07-20 16:44:51 +0800

    添加 popup

--- modules/page-display/modal/index.js
+++ modules/page-display/modal/index.js
@@ -1,7 +1,8 @@
 import init from './init_modal';
 
 import {select} from "./modal_select";
-import {alert, confirm, confirmPwd} from './modal_popup';
+import {alert, confirm, confirmPwd} from './modal_prompt_box';
+import popup from './modal_popup';
 
 import initLoading from './init_loading';
 import loading from './modal_loading';
@@ -9,8 +10,9 @@ import loading from './modal_loading';
 export default {
     init,//初始化页面
 
-
     select,
+    popup,
+
     alert,
     confirm,
     confirmPwd,
---
+++ modules/page-display/modal/modal_prompt_box.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import {View, StyleSheet, Text, TouchableOpacity, TextInput} from 'react-native';
+import language from '../language';
+import theme from '../theme';
+import redux from '../../data-storage/redux';
+
+import Modals from './init_modal';
+
+const timer = 200;
+
+const show = (style, data) => {
+    redux.update(Modals.reduxName, {
+        type: 1,
+        display: true,
+        style: style,
+        data: data,
+    });
+};
+
+const hide = () => {
+    redux.update(Modals.reduxName, {
+        type: 1,
+        display: false,
+        style: {},
+        data: null,
+        onPress: null
+    });
+};
+
+const goOn = (func) => {
+    redux.listen(Modals.reduxName, res => {
+        if (res.continue && typeof func === "function") func();
+    })
+};
+
+//初始化
+const PromptInit = ({title, content, btns}) => {
+    const css = theme.get();
+    const style = styles(css);
+
+    //内容
+    let contentView = <View/>;
+    if (typeof content === 'string')
+        contentView = <Text style={style.popupContentText}>{content}</Text>;
+    else contentView = content;
+
+    return <View style={style.popupView}>
+        <Text style={style.popupTitle}>{title}</Text>
+        <View style={style.popupContent}>{contentView}</View>
+        <View style={style.popupBtns}>{btnsView(btns)}</View>
+    </View>
+};
+//按钮
+const btnsView = (btnsArr) => {
+    const css = theme.get();
+    const style = styles(css);
+    return btnsArr.map((item, key) => {
+        let border = {};
+        if (key > 0) border = item.border ?? style.btnsViewBorder;
+        return <TouchableOpacity key={key} style={[style.btnsView, border]} onPress={() => {
+            if (typeof item.onPress === 'function') item.onPress();
+            hide();
+        }}>
+            <Text style={[item.styles, {textAlign: 'center'}]}>{item.text}</Text>
+        </TouchableOpacity>
+    });
+};
+
+//alert弹出框
+const alert = (content = '', params = {title: undefined, btnText: undefined}) => {
+    const css = theme.get();
+    const style = styles(css);
+    return new Promise((resolve => {
+        let title, btnText;
+        if (!params.title) title = '提示';
+        else title = params.title;
+        let contentData = content;
+        if (!params.btnText) btnText = '确定';
+        else btnText = params.btnText;
+
+        const lang = language.all('modal');
+        if (lang) {
+            if (!params.title) title = lang['title_tips'];
+            if (!params.btnText) btnText = lang['btn_sure'];
+        }
+        let btns = [{
+            text: btnText,
+            onPress: () => {
+                goOn(() => resolve(true));
+            }
+        }];
+        const popup = PromptInit({title, content: contentData, btns});
+        show(style.popup, popup);
+    }))
+};
+
+const confirm = (content = '', params = {title: undefined, btns: [{}, {}]}) => {
+    const css = theme.get();
+    const style = styles(css);
+    return new Promise((resolve, reject) => {
+        let title;
+        if (!params.title) title = '提示';
+        else title = params.title;
+        if (!params.btns) params.btns = [{}, {}];
+        let contentData = content;
+        let btnDefs = [{
+            text: '确定', onPress: () => {
+                goOn(() => resolve());
+            }
+        }, {
+            text: '取消', onPress: () => {
+                goOn(() => reject());
+            }
+        }];
+
+        params.btns.map((item, key) => {
+            if (!btnDefs[key]) return;
+            if (!item.text) item.text = btnDefs[key].text;
+            if (typeof item.onPress !== "function") item.onPress = () => btnDefs[key].onPress();
+        });
+
+        //多语言
+        const lang = language.all('modal');
+        if (lang) {
+            if (!params.title) title = lang['title_tips'];
+            let btnTexts = [lang['btn_sure'], lang['btn_cancel']];
+            params.btns.map((item, key) => {
+                if (!btnDefs[key]) return;
+                if (!item.text) item.text = btnTexts[key];
+            });
+        }
+
+        const popup = PromptInit({title, content: contentData, btns: params.btns});
+        show(style.popup, popup);
+    });
+};
+
+const confirmPwd = (params = {title: null}) => {
+    const css = theme.get();
+    const style = styles(css);
+    let valText = '';
+
+    params.title = params.title ?? '密码';
+    let contentData = <TextInput style={style.confirmPwd} password={true} secureTextEntry={true}
+                                 autoFocus={true} onChangeText={text => valText = text.toString()}/>;
+
+    return new Promise((resolve, reject) => {
+        return confirm(contentData, {
+            title: params.title, btns: [{onPress: () => goOn(() => resolve(valText))},
+                {onPress: () => goOn(() => reject(valText))}]
+        })
+    });
+
+
+};
+
+const styles = (css) => StyleSheet.create({
+    popup: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    popupView: {
+        width: css.width - 15 * 7,
+        backgroundColor: '#efefef',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        overflow: 'hidden',
+        // elevation: 5,
+    },
+    popupTitle: {
+        fontSize: 18,
+        fontWeight: '700',
+        textAlign: 'center',
+        height: 50,
+        lineHeight: 50,
+        color: '#000',
+    },
+    popupContent: {
+        paddingBottom: 15,
+        paddingHorizontal: 15,
+        maxHeight: css.height - 200,
+    },
+    popupContentText: {
+        color: '#666',
+        textAlign: 'center',
+        lineHeight: 22,
+    },
+    popupBtns: {
+        borderTopWidth: 1,
+        borderTopColor: '#e1e1e1',
+        height: 50,
+        flexDirection: 'row-reverse',
+        justifyContent: 'space-between',
+    },
+    btnsView: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        flexGrow: 1,
+    },
+    btnsViewBorder: {
+        borderRightColor: '#e1e1e1',
+        borderRightWidth: 1,
+    },
+    confirmPwd: {
+        height: 40,
+        paddingHorizontal: 15,
+        borderWidth: 1,
+        borderColor: '#e1e1e1',
+        borderRadius: 5,
+        backgroundColor: '#fff',
+        color: '#808080',
+    }
+});
+
+export {
+    alert,
+    confirm,
+    confirmPwd,
+}
