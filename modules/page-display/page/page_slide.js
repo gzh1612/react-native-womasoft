d1e6d28dcd2902fd533a8576bbe8f854b723e036
gzh1612 <gzh1215@hotmail.com>
2020-08-03 15:03:43 +0800

    修改 page.slide

--- modules/page-display/page/page_slide.js
+++ modules/page-display/page/page_slide.js
@@ -12,8 +12,10 @@ export default class PageSlide extends Component {
             style: props.style,
             slideStyle: props.slideStyle,
             onRefresh: props.onRefresh,
+            onPaging: props.onPaging,
             refreshing: props.refreshing,
             log: props.log ?? false,
+            loadingPaging: props.loadingPaging ?? false,
         }
     }
 
@@ -21,11 +23,14 @@ export default class PageSlide extends Component {
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         if (nextProps.children === this.state.children &&
             nextProps.refreshing === this.state.refreshing &&
+            nextProps.loadingPaging === this.state.loadingPaging &&
+            nextProps.onPaging === this.state.onPaging &&
             JSON.stringify(nextProps.style) === JSON.stringify(this.state.style) &&
             JSON.stringify(nextProps.slideStyle) === JSON.stringify(this.state.slideStyle)) return false;
         if (this.state.log) {
             console.log('children', nextProps.children === this.state.children);
             console.log('refreshing', nextProps.refreshing === this.state.refreshing);
+            console.log('loadingPaging', nextProps.loadingPaging === this.state.loadingPaging);
             console.log('style', JSON.stringify(nextProps.style) === JSON.stringify(this.state.style));
             console.log('slideStyle', JSON.stringify(nextProps.slideStyle) === JSON.stringify(this.state.slideStyle));
             console.log('==================================');
@@ -35,12 +40,21 @@ export default class PageSlide extends Component {
             style: nextProps.style ?? this.state.style,
             slideStyle: nextProps.slideStyle ?? this.state.slideStyle,
             refreshing: nextProps.refreshing,
+            loadingPaging: nextProps.loadingPaging,
+            onPaging: nextProps.onPaging,
         });
         return true;
     }
 
     render() {
         return <ScrollView style={this.state.slideStyle ?? {}}
+                           onScroll={(event) => {
+                               let currHeight = this.state.height + event['nativeEvent']['contentOffset'].y,
+                                   totalHeight = event['nativeEvent']['contentSize'].height;
+                               if ((totalHeight - 1) < currHeight && this.state.loadingPaging) {
+                                   if (typeof this.state.onPaging === 'function') this.state.onPaging();
+                               }
+                           }}
                            onLayout={(event) => this.setState({height: event['nativeEvent']['layout'].height})}
                            refreshControl={
                                <RefreshControl refreshing={this.state.refreshing ?? false}
