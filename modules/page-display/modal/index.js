38a11eb277fd1e55c9d86d9cb729ef8ad6f79932
gzh1612 <gzh1215@hotmail.com>
2020-08-07 11:30:55 +0800

    修改 modal.popup

--- modules/page-display/modal/index.js
+++ modules/page-display/modal/index.js
@@ -1,26 +1,26 @@
-import init from './init_modal';
+import init_modals from './init_modal';
+import init_popup from './init_popup';
+import init_loading from './init_loading';
+
+
+import modal_popup from './modal_popup';
+import modal_loading from './modal_loading';
+
 
 import {select} from "./modal_select";
 import {alert, confirm, confirmPwd} from './modal_prompt_box';
 
-import initLoading from './init_loading';
-import loading from './modal_loading';
-
-import initPopup from './init_popup';
-import popup from './modal_popup';
-
 export default {
-    init,//初始化页面
+    initModal: init_modals,//初始化页面
+    initPopup: init_popup,
+    initLoading: init_loading,
 
     select,
-
     alert,
     confirm,
     confirmPwd,
 
-    initLoading,
-    loading,
+    popup: modal_popup,
+    loading: modal_loading,
 
-    initPopup,
-    popup,
 };
