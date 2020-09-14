3f7230465f11bde24bc12cf6fa700e67872b5367
gzh1612 <gzh1215@hotmail.com>
2020-09-14 13:53:09 +0800

    修改 page

--- modules/page-display/page/page_a.js
+++ modules/page-display/page/page_a.js
@@ -67,7 +67,7 @@ export default class PageA extends Component {
         else style.fontSize = css.font.size ?? 14;
         if (state.weight) style.fontWeight = state.weight ?? 'normal';
         if (state.color) style.color = state.color ?? css.font.color ?? '#808080';
-        else style.color = css.font.color ?? '#808080';
+        else style.color = styles[1].color ?? css.font.color ?? '#808080';
 
         if (state.lineHeight) style.lineHeight = state.lineHeight;
         if (state.marginTop) style.marginTop = state.marginTop;
--- modules/page-display/page/page_text.js
+++ modules/page-display/page/page_text.js
@@ -57,7 +57,7 @@ export default class PageText extends Component {
         else style.fontSize = css.font.size ?? 14;
         if (state.weight) style.fontWeight = state.weight ?? 'normal';
         if (state.color) style.color = state.color ?? css.font.color ?? '#808080';
-        else style.color = css.font.color ?? '#808080';
+        else style.color = styles[1].color ?? css.font.color ?? '#808080';
 
         if (state.lineHeight) style.lineHeight = state.lineHeight;
         if (state.marginTop) style.marginTop = state.marginTop;
